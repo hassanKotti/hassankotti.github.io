e5ebdb249d48c4e9d77211d7e91e89823bec4da4
hassankotti <hkotti95@gmail.com>
2021-12-11 21:51:57 +0400

    build

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import "./App.css";
 import "./index.css";
 import "./style.css";
 import Landing from "./pages/Landing";
-import NoMatch from "./pages/NoMatch";
 import { BrowserRouter,Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter basename='/'>
-      {/* <Route path='*' component={NoMatch} /> */}
-      <Route path='/' exact component={Landing} />
+      <Route path='*' exact component={Landing} />
     </BrowserRouter>
   );
 }
